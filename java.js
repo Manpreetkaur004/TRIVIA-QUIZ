88b5bf2e3052106c219a203d393e4f73c1b0c9d7
Manpreet Kaur <riyatmanpreet004@gmail.com>
2024-07-03 21:38:01 +0530

    .vscode\TRIVIA QUIZ

---
+++ java.js
@@ -0,0 +1,14 @@
+const quizData = [
+    {
+      question 1: "What is your favourite colour ?"
+      a: "red"
+      b: "black"
+      c: "purple"
+      d: "pink"
+
+    }
+    {question 2:"Do you watch netflix?"
+        a: "no
+        b:"yes"
+    }
+  
